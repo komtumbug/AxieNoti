9434b546ced32f65d8df7ff5ba199d61e40c8397
khaijeaw65 <fieldlegoza@gmail.com>
2021-10-25 20:26:20 +0700

    ใส่ axie ใน background

--- CreateImage.py
+++ CreateImage.py
@@ -1,17 +1,32 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 import requests
 from io import BytesIO
 import os
 
-imgname = "axie.jpg"
+imgname = "axie.png"
+testText = 'WTF'
+font = './fonts/Roboto-Regular-ttf'
+background = Image.new("RGB", (500, 600), ImageColor.getrgb("#242735"))
+bg_w, bg_h = background.size
 
 def GenImg():
     response = requests.get("https://storage.googleapis.com/assets.axieinfinity.com/axies/7479197/axie/axie-full-transparent.png")
     img = Image.open(BytesIO(response.content))
-    imgresize = img.resize((200,150))
-    rgb_img = imgresize.convert('RGB')
-    rgb_img.save(imgname)
+    
+    imgresize = img.resize((600, 450))
+    rgb_img = imgresize.convert('RGBA')
+
+    rgb_img.save(imgname, 'png')
+
+    img_w, img_h = rgb_img.size
+
+    offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+    background.paste(rgb_img, offset, mask=rgb_img)
+    background.save('out.png')
+    
 
 
 def DelImg():
     os.remove(imgname)
+
+GenImg()
